c18e82324c945af787a88b890774020c1d250090
Guisn25 <127779512+Guisn25@users.noreply.github.com>
2023-10-02 20:34:37 -0300

    Delete projeto2.v

--- projeto2.v
+++
@@ -1,9 +0,0 @@
-module projeto2(Clock, Reset, a,b,c,d,e,f,g);
-	input Clock, Reset;
-	output a, b, c, d, e, f, g;
-	wire [2:0] bit;
-	
-	ContadorAssincrono cont1(.Clock(Clock), .Reset(Reset), .S(bit));
-	DecodificadorBCD decod1(.Z(bit[0]), .Y(bit[1]), .X(bit[2]), .W(0), .a(a), .b(b), .c(c), .d(d), .e(e), .f(f), .g(g));
-endmodule
-	
